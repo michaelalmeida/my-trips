7cc343714271f2b829602c1d90fd9b2dcdf92053
michaelalmeida <michaelralmeida@gmail.com>
2022-09-18 18:07:47 +0200

    fix: footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -7,13 +7,7 @@ export const Footer = () => (
             <a href="https://github.com/michaelalmeida" target="_blank" rel="noopener noreferrer">
                 Michael
             </a>
-            . Curious about me?{' '}
-            <a
-                href="https://www.linkedin.com/in/michaelralmeida/"
-                target="_blank"
-                rel="noopener noreferrer">
-                My LinkedIn
-            </a>
+            .
         </Paragraph>
     </FooterWrapper>
 );
